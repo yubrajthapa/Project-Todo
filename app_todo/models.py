002196353d1c3662f9917908b803af1757313324
yubrajthapa <yubrajthapa742@gmail.com>
2022-07-22 00:51:26 +0545

    Initial commit

---
+++ app_todo/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class UserDetails(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.IntegerField()
+    is_verified = models.BooleanField()
+
+    def __str__(self):
+        return self.first_name
+    
+    class Meta:
+        db_table = "User_Details"
+
+class UserTodo(models.Model):
+    todo = models.CharField(max_length=200)
+    desc = models.CharField(max_length=250)
+    expired_date = models.DateField()
+    is_completed = models.BooleanField()
+    usertodo = models.ForeignKey(UserDetails, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.todo
+
+    class Meta:
+        db_table = "UserTodo"
