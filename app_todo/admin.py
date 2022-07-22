ca1480c2cae5434ff87547674073bbc44b1f24ea
yubrajthapa <yubrajthapa742@gmail.com>
2022-07-22 14:26:20 +0545

    Creating forms for User Registration & Login

--- app_todo/admin.py
+++ app_todo/admin.py
@@ -3,7 +3,7 @@ from app_todo.models import *
 
 # Register your models here.
 class Details(admin.ModelAdmin):
-    list_display = ('first_name', 'middle_name', 'last_name', 'email', 'contact')
+    list_display = ('first_name', 'middle_name', 'last_name', 'email', 'password', 'contact')
 
 admin.site.register(UserDetails,Details)
 admin.site.register(UserTodo)
